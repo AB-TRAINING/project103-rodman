b5326cf1a4d3a001ba9130faae7453e20ced6fc6
rdmnlee <rodmanperezlee@gmail.com>
2021-03-16 21:46:42 +0800

    Feat: Add calculator and calendar

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>Main</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/navbar-fixed/">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <link href="/assets/css/main.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <a class="navbar-brand" href="#">My page</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="main.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/pages/calculator/calculator.php">Calculator</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/pages/calendar/calendar-index.php">Calendar</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<main role="main" class="container">
+  <div class="jumbotron">
+    <h1>Calculator</h1>
+    <p class="lead">This is the calculator application.</p>
+    <a class="btn btn-lg btn-primary" href="/pages/calculator/calculator.php" role="button">Go to Calculator &raquo;</a>
+  </div>
+
+  <div class="jumbotron">
+    <h1>Calendar</h1>
+    <p class="lead">This is the calendar application.</p>
+    <a class="btn btn-lg btn-primary" href="/pages/calendar/calendar-index.php" role="button">Go to Calendar &raquo;</a>
+  </div>
+</main>
+
+      
+  </body>
+</html>
